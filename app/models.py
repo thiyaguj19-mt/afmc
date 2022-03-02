08bbd65539d4d3f107d55d37258ad8793fac09f4
Thiyagarajan Mohan <thiyaguj19@gmail.com>
2022-03-01 20:27:33 -0800

    data model

--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ class Provider(models.Model):
         ('Nurse', 'Nurse'),
     ]
     type = models.CharField(max_length=10, choices=TYPES, default='Doctor')
-    license_number = models.CharField(max_length=20, help_text="enter license number", blank=True)
+    license_number = models.CharField(max_length=20, help_text="enter license numbers", blank=True)
 
     def __str__(self):
         return f'{self.first_name} | {self.last_name}'
