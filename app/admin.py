8ac78238f1f785bf0a81432e248e55d58c5febcf
Thiyagarajan Mohan <thiyaguj19@gmail.com>
2022-03-02 22:32:28 -0800

    updated data model

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 from .models import *
 
-# Register your models here.
-admin.site.register(Provider)
-admin.site.register(Volunteer)
-admin.site.register(Activities)
-admin.site.register(Credentialing)
+class ProviderAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'type', 'email', 'status')
+    list_filter = ('type', 'status')
+admin.site.register(Provider, ProviderAdmin)
+
+class ActivitiesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'assigned_to', 'status')
+    list_filter = ('status', 'assigned_to')
+admin.site.register(Activities, ActivitiesAdmin)
+
+class VolunteerAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'status')
+    list_filter = ('status', 'first_name')
+admin.site.register(Volunteer, VolunteerAdmin)
+
+class CredentialingAdmin(admin.ModelAdmin):
+    list_display = ('type', 'year', 'status', 'license_duedate', 'provider')
+    list_filter = ('type', 'status', 'license_duedate', 'year', 'provider')
+admin.site.register(Credentialing, CredentialingAdmin)
--- app/models.py
+++ app/models.py
@@ -1,7 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Volunteer(models.Model):
+
+    first_name = models.CharField(max_length=100, help_text="enter first name")
+    last_name = models.CharField(max_length=100, help_text="enter last name")
+    email = models.EmailField(max_length=100, help_text="enter email")
+    phone = models.CharField(max_length=100, help_text="enter phone number")
+    V_STATUS = [
+        ('Active', 'Active'),
+        ('Inactive', 'Inactive'),
+    ]
+    status = models.CharField(max_length=20, choices=V_STATUS, default='Active')
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} | {self.status}'
+
 class Provider(models.Model):
+
     first_name = models.CharField(max_length=100, help_text="enter first name")
     middle_name = models.CharField(max_length=100, help_text="enter middle name", blank=True)
     last_name = models.CharField(max_length=100, help_text="enter last name")
@@ -13,20 +29,17 @@ class Provider(models.Model):
     ]
     type = models.CharField(max_length=10, choices=TYPES, default='Doctor')
     license_number = models.CharField(max_length=20, help_text="enter license numbers", blank=True)
-
-    def __str__(self):
-        return f'{self.first_name} | {self.last_name}'
-
-class Volunteer(models.Model):
-    first_name = models.CharField(max_length=100, help_text="enter first name")
-    last_name = models.CharField(max_length=100, help_text="enter last name")
-    email = models.EmailField(max_length=100, help_text="enter email")
-    phone = models.CharField(max_length=100, help_text="enter phone number")
+    P_STATUS = [
+        ('Active', 'Active'),
+        ('Inactive', 'Inactive'),
+    ]
+    status = models.CharField(max_length=20, choices=P_STATUS, default='Active')
 
     def __str__(self):
         return f'{self.first_name} | {self.last_name}'
 
 class Activities(models.Model):
+
     name = models.CharField(max_length=100, help_text="activity name")
     description = models.TextField(max_length=600, help_text="activity description")
     assigned_to = models.ForeignKey(Volunteer, on_delete=models.SET_NULL, null=True, blank=True)
@@ -38,16 +51,19 @@ class Activities(models.Model):
         ('completed', 'completed'),
     ]
     status = models.CharField(max_length=20, choices=TASK_STATUS, default='backlog')
+    start_date = models.DateField(help_text = 'enter start date',  null=True, blank=True)
+    end_date = models.DateField(help_text = 'enter completed date', null=True, blank=True)
 
     def __str__(self):
         return f'Task Name - {self.name} | Task Status - {self.status} | Assigned To - {self.assigned_to}'
 
 class Credentialing(models.Model):
+
     TYPES = [
         ('Intial', 'Intial'),
         ('Reappointment', 'Reappointment'),
     ]
-    type = models.CharField(max_length=20, choices=TYPES, default='Reappointment')
+    type = models.CharField(max_length=20, choices=TYPES)
     start_date = models.DateField(help_text = 'enter start date')
     end_date = models.DateField(help_text = 'enter completed date', blank=True)
     year = models.IntegerField(help_text = 'enter year of Credentialing')
@@ -60,5 +76,8 @@ class Credentialing(models.Model):
     ]
     status = models.CharField(max_length=20, choices=C_STATUS, default='backlog')
     provider = models.ForeignKey(Provider, on_delete=models.SET_NULL, null=True)
-    license_duedate = models.DateField(help_text = 'enter start date', blank=True)
+    license_duedate = models.DateField(help_text = 'enter start date', null=True, blank=True)
     remarks = models.TextField(max_length=600, help_text="remarks/comments", blank=True)
+    peer_reviewer_1 = models.CharField(max_length=100, help_text="enter peer review email address", blank=True)
+    peer_reviewer_2 = models.CharField(max_length=100, help_text="enter peer review email address", blank=True)
+    peer_reviewer_3 = models.CharField(max_length=100, help_text="enter peer review email address", blank=True)
